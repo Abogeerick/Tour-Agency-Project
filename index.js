d1aaf3f97b919320087c580681711748a242879e
Abogeerick <abogeerick@gmail.com>
2023-07-07 05:29:40 +0300

    Added a navbar in the html file and styled it in the index.css

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
   function showPackages(id, packageId, button) {
     fetch(`http://localhost:3000/packages/${id}`)
       .then(response => {
-        if (!response.ok) {
-          throw new Error("Network response was not OK");
-        }
         return response.json();
       })
       .then(showPackages => {
